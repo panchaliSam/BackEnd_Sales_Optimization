d699d9e157c75f351eba33a7322b2183ba677d11
panchaliSam <panchalisam20030117@gmail.com>
2024-09-04 12:41:32 +0530

    Implement SalesRecords routes

---
+++ server/routes/SalesRecord.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const SalesRecordController = require('../controller/SalesRecord');
+
+//Create a new sales record
+router.post('/', SalesRecordController.createSalesRecord);
+
+//Read all sales records
+router.get('/', SalesRecordController.getAllSalesRecords);
+
+//Read a single sales records
+router.get('/:id', SalesRecordController.getSalesRecordById);
+
+// Read a single sales record by ID
+router.get('/:id', SalesRecordController.getSalesRecordById);
+
+// Update a sales record by ID
+router.put('/:id', SalesRecordController.updateSalesRecordById);
+
+// Delete a sales record by ID
+router.delete('/:id', SalesRecordController.deleteSalesRecordById);
+
+module.exports = router;
