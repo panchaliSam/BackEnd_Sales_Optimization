7b7470727437003f43722c14f6de62954e28ce51
panchaliSam <panchalisam20030117@gmail.com>
2024-09-06 14:51:15 +0530

    Chnges to salesRecord model

--- server/models/salesRecord.model.js
+++ server/models/salesRecord.model.js
@@ -1,28 +1,25 @@
 const mongoose = require('mongoose');
 
-//Define the available options for dropdown fields
-//Sample categories
+// Define the available options for dropdown fields
 const productCategories = ['Clothing', 'Footwear', 'Accessories'];
-//Sample provinces of Sri Lanka
 const sriLankanProvinces = [
-    'Western', 'Central', 'Southern', 'Northen',
+    'Western', 'Central', 'Southern', 'Northern',
     'Eastern', 'North Western', 'North Central',
     'Uva', 'Sabaragamuwa'
 ];
-//Sample Brands
 const productBrands = ['BrandA', 'BrandB', 'BrandC', 'BrandD', 'BrandE'];
 
 const salesRecordSchema = new mongoose.Schema(
     {
         date: {
             type: Date,
-            required: true
+            required: true,
+            unique: true  // Ensure the date is unique
         },
         salesQuantity: {
             type: Number,
             required: true
         },
-
         productCategory: {
             type: String,
             enum: productCategories,
@@ -33,12 +30,16 @@ const salesRecordSchema = new mongoose.Schema(
             enum: productBrands,
             required: true
         },
-
         customerLocation: {
             type: String,
             enum: sriLankanProvinces,
             required: true
         }
-});
+    },
+    { timestamps: true }
+);
+
+// Create a unique index on the date field to prevent duplicate dates
+salesRecordSchema.index({ date: 1 }, { unique: true });
 
 module.exports = mongoose.model('SalesRecord', salesRecordSchema);
