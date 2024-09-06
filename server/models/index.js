5bfac5c6b83b7c5334367218e5a13609bcc496c3
kumodi bogahawatte <kumodib@gmail.com>
2024-09-06 12:07:10 +0530

    Created customer record model, controller, and API endpoints for CRUD operations.

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,10 @@
 const salesRecords = require('./salesRecord.model');
+const customerRecords = require('./customerRecod.model');
 
 module.exports = {
     salesRecords
+};
+
+module.exports = {
+    customerRecords
 };
