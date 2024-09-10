3272c87739b6d35105ce7a58aa73afefb7c0abe1
panchaliSam <panchalisam20030117@gmail.com>
2024-09-10 15:41:45 +0530

    Complete jwt auth

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bycryptjs');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = mongoose.Schema({
     userName: {
@@ -17,18 +17,26 @@ const UserSchema = mongoose.Schema({
         required: true,
         trim: true,
     },
-})
-
-//Hash password before saving the user
-UserSchema.pre('save', function (next) {
-    if (this.isModified('password')) {
-        this.password = bcrypt.hashSync(this.password, 10);
-    }
-    next();
 });
 
-UserSchema.methods.comparePassword = function (password) {
-    return bcrypt.compareSync(password, this.password);
+// Hash password before saving
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('password') || this.isNew) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password, salt);
+            next();
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        return next();
+    }
+});
+
+// Compare the provided password with the stored password
+UserSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('UserSchema', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
--- server/routes/api/userRecord.route.js
+++ server/routes/api/userRecord.route.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const UserRecordController = require('../../controller');
+const UserRecordController = require('../../controller/user.controller');
 
+// Register a new user
 router.post('/register', UserRecordController.registeredUser);
-router.post('/login', UserRecordController.loginUser);
-router.post('/dashboard', UserRecordController.userDashboard);
-router.post('/:id', UserRecordController.getUserById);
 
+// Log in a user
+router.post('/login', UserRecordController.loginUser);
+
+// Get user dashboard
+// router.post('/dashboard', UserRecordController.userDashboard);
+
+// Get user by ID
+router.get('/:id', UserRecordController.getUserById);
 
 module.exports = router;
